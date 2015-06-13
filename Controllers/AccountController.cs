b36def0be067a64171bfd2c7d30b649933099e21
bcsf11m119 <bcsf11m119@pucit.edu.pk>
2015-06-13 23:52:29 +0500

    change made for junior
    junior

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,226 @@
+﻿//using project.Models;
+using EADproject.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class AccountController : Controller
+    {
+        Database1Entities1 db = new Database1Entities1();
+
+        //int count1 = 0;
+        //int count2 = 0;
+        IUser i;
+        User user=null;
+        public AccountController(IUser u)
+        {
+            i = u;
+
+            //User u = db.Users.Last<User>();
+            //count1 = db.Users.Last<User>().Uid;
+            //u.Uid;
+        }
+        //File path;
+        public ViewResult Login()
+        {
+            ///ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //     [AllowAnonymous]
+        public ActionResult PlayList(User u1)
+        {
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            return View("PlayList");
+        }
+
+        public ViewResult Upload()
+        //public ViewResult Upload(User u)
+        {
+            //ViewBag.us = this.user;
+            //this.user = ViewBag.us;
+            //List<Video> vl = u.Videos.ToList();
+            //ViewBag.list = vl;
+
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            return View("Upload");
+        }
+
+        public ActionResult Register()
+        {
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            return View();
+            //return "";
+        }
+        [HttpPost]
+        //public ViewResult CreateUser(User u)
+        public RedirectToRouteResult CreateUser(User u)
+        {
+            //       db.Users.Attach(u);
+            //db.Users.First(u);
+            //if (db.Users.First())
+            {
+
+            }
+            //User s = db.Users.Find(X = u.Uid);
+            // u.Uid = ++count1;
+            //u.Email = Request["email"];
+            if (i.Add(u))
+            {
+                Playlist p = new Playlist();
+                p.Uid = u.Uid;
+                p.Name = "Defualt";
+                p.UpdationDate = System.DateTime.Now;
+                p.CreationDate = System.DateTime.Now;
+             //   i.AddPlayList(p);
+                //return View();
+                user = u;
+                this.Session["sessionString"]=u;
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+                //return RedirectToAction("/Account/PlayList");
+                return RedirectToAction("/PlayList");
+                
+            }
+            else
+                return RedirectToAction("LoginError");
+            //db.Users.Add(u);
+
+            //db.SaveChanges();
+        }
+        [HttpPost]
+        public ViewResult Verify(User u)
+        {
+            //User s = db.Users.Find(u.Uid);
+
+            User s = db.Users.First(x=>x.Email.Equals(u.Email));
+            if (s == null)
+            {
+                return View("Error");
+            }
+            if (!s.password.Equals(u.password))
+            {
+                return View("Error");
+            }
+            //this.user = s;
+            //System.Web.HttpContext.Current.Session["sessionString"] = s;
+            this.Session["sessionString"] = s;
+            //this.Session = s;
+            //Video v = db.Videos.Find(x => x.Uid=u.Uid);
+            //Video v = db.Videos.Find(x=> x.Uid= u.Uid);
+            //PlayList p = db.PlayLists.Find(u.Uid);
+            //List<Playlist> l = db.Playlists.ToList<Playlist>();
+            //List<Video> vl= (List<Video>) l[0].Videos;
+
+            List<Video> vl = s.Videos.ToList();//l[0].Videos.ToList();
+            //var q = from x in l where x.Uid == u.Uid select x;
+            //List<PlayList> c = q.ToList<PlayList>();
+            //var q1 = from y in c where y.Vid
+            ViewBag.us = this.user;
+            ViewBag.list = vl;
+            return View("PlayList");
+
+
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(  HttpPostedFileBase file)
+        {
+
+            //User u = ViewBag.u;
+            //User u=ViewBag.us;
+            User u = (User)this.Session["sessionString"];
+            Video v = new Video();
+            if (u != null)
+            {
+                //i.AddVideo
+                // Verify that the user selected a file
+                if (file != null && file.ContentLength > 0)
+                {
+                    // extract only the fielname
+                    var fileName = Path.GetFileName(file.FileName);
+                    v.Name = fileName;
+                    v.Description = Request["description"];
+                    v.Date = System.DateTime.Now;
+                    string ext = Path.GetExtension(file.FileName);
+                    if (!ext.Equals(".mp4") && !ext.Equals(".mp3"))
+                    {
+                        //String err="Please  Upload Valid File";
+                        return View("Error");
+                    }
+                    User s=db.Users.First(x => x.Email.Equals(u.Email));
+                    //Playlist p=s.Playlists.First(x=>x.Uid==s.Uid);
+                  
+                    //Playlist p1 = db.Playlists.First();
+                    //Video [] v1 =(Video [] )p1.Videos;
+                    //v.Pid = p.Pid;
+                    v.Uid = s.Uid;
+                    v.ext = ext;
+                    i.AddVideo(v);
+                    //p.Videos;
+                    //Array p=s.Playlists.ToArray();
+                   //Playlist l ;
+                    
+
+
+                    // store the file inside ~/App_Data/uploads folder
+                    //var path = Path.Combine(Server.MapPath("~/App_Data/upload"), fileName);
+                    var path = Path.Combine(Server.MapPath("~/upload"), fileName);
+                    file.SaveAs(path);
+                    //v.Vid = ++count2;
+                    u = db.Users.First(x => x.Email.Equals(u.Email));
+                    this.Session["sessionString"] = u;
+                    ViewBag.us = u;
+                    List<Video> vl = u.Videos.ToList();//l[0].Videos.ToList();
+                    //var q = from x in l where x.Uid == u.Uid select x;
+                    //List<PlayList> c = q.ToList<PlayList>();
+                    //var q1 = from y in c where y.Vid
+                    ViewBag.list = vl;
+                    return View("PlayList");
+
+                }
+            }
+            // redirect back to the index action to show the form once again
+            //return RedirectToAction("/Home/Index");
+            //List<Video> vl = s.Videos.ToList();//l[0].Videos.ToList();
+            //var q = from x in l where x.Uid == u.Uid select x;
+            //List<PlayList> c = q.ToList<PlayList>();
+            //var q1 = from y in c where y.Vid
+            //ViewBag.list = vl;
+            return View("PlayList");
+        }
+
+            public JsonResult uploadGet()
+            {
+                return this.Json(true,JsonRequestBehavior.AllowGet);
+            }
+            public JsonResult SubmitGet(string n)
+            {
+                return this.Json(true, JsonRequestBehavior.AllowGet);
+            }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,187 @@
+﻿using EADproject.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Models
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        Database1Entities1 db = new Database1Entities1();
+ //       Database1Entities1 db = new Database1Entities1();
+        public ViewResult Index()
+        {
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+        //public ViewResult Play(Video v)
+        public ViewResult Play(string name)
+
+        {
+
+            Video v = new Video();
+            Video v1 = db.Videos.First(x => x.Name.Equals(name));
+            v.Name = v1.Name;
+            //v.Name = v.Name + v1.ext;
+
+
+            //v1.Name = name;
+            // v.Name;//name;
+            //ViewBag.name;
+            //System.Console.WriteLine(name);
+            //string n = v.Name;
+        //    string n = name;
+
+                //Request["videoName"];
+                
+                //name;
+                //"Hands in the Air.mp4";
+            
+            // name;    //v.Name;
+                //ViewBag.name;  //Request["videoName"];
+         //   n=n.Insert(n.Length, ".mp4");
+          //  n = n + ".mp4";
+
+            //return View(n);
+            //v.Name = v.Name + ".mp4";
+            //v1.Name = v1.Name + ".mp4";
+            //v1.Name = "preview-upbeat-feeling.mp3";
+            User u = (User)this.Session["sessionString"];
+            if (u != null)
+            {
+                ViewBag.us = u;
+
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+            return View(v);
+        }
+        
+        public ViewResult Search()
+        {
+            User u = (User)this.Session["sessionString"];
+            ViewBag.us = u;
+            if (u != null)
+            {
+                List<Video> vl = u.Videos.ToList();
+                ViewBag.list = vl;
+            }
+
+            string s1 = Request["search"];
+                List<Video> s = new List<Video>();
+            if (s1.Equals(""))
+            {
+                //return View(db.Videos.ToList());
+                //return View("/Account/Error");
+                //return View(db.Videos.ToList());
+                return View(db.Videos.ToList<EADproject.Models.Video>());
+                //return View(db.Videos.ToArray());
+            }
+            else
+            {
+                //db.Videos.First(x => x.Name.Equals(s1));
+                List<Video> l = db.Videos.ToList();
+                
+                foreach( var i in l)
+                {
+                    if (i.Name.Contains(s1))
+                    //if(i.Name.)
+                    {
+                        s.Add(i);
+                        continue;
+                    }
+                    string[] items = s1.Split(' ');
+                    foreach (var j in items)
+                    {
+                        if (i.Name.Contains(j))
+                        {
+                            s.Add(i); break;
+                        }
+                    }
+                }
+            }
+
+            //Video  v=db.Videos.Find("name like \'"+s1+"\'");
+            //Vide,o [] v =db.Videos.Select<Video,Video[]>("name like \'" + s1 + "\'");
+            //Video v = db.Videos.First(23);
+            //Video v = db.Videos.SelectMany
+
+            //var q = from x in db where 
+
+            //return View("Search.cshtml");
+            return View(s);
+            // else return View("string not found");
+        }
+
+        public ViewResult falak()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult Atif()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult demi()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult arijit()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult farhan()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("farhan");
+        }
+
+
+
+
+    }
+
+
+}
